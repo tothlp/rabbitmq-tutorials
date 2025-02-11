16fa1072981df8378d0a9e79fd52609fe17f82f3
Tóth László <laszlo.toth@sagemcom.hu>
2025-02-11 12:16:09 +0100

    init

---
+++ src/main/kotlin/hu/tothlp/tutorials/rabbitmq_tutorials/RabbitmqTutorialsApplication.kt
@@ -0,0 +1,11 @@
+package hu.tothlp.tutorials.rabbitmq_tutorials
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RabbitmqTutorialsApplication
+
+fun main(args: Array<String>) {
+	runApplication<RabbitmqTutorialsApplication>(*args)
+}
---
+++ src/test/kotlin/hu/tothlp/tutorials/rabbitmq_tutorials/RabbitmqTutorialsApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.tothlp.tutorials.rabbitmq_tutorials
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RabbitmqTutorialsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
